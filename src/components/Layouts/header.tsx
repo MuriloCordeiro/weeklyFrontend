b63cfd8d424b3dee945b7cdcc4d4def53d9e28f0
Murilo <murilo.ramos@rspneus.com.br>
2022-11-21 17:03:51 -0300

    chore: new commit

--- src/components/Layouts/header.tsx
+++ src/components/Layouts/header.tsx
@@ -7,29 +7,23 @@ import {
   TabPanel,
   TabPanels,
   Tabs,
+  Avatar,
+  Wrap,
+  WrapItem,
 } from "@chakra-ui/react";
 import Home from "../../pages/homepage";
 
 export default function Header() {
   return (
     <Flex h="auto" w="auto">
-      <Tabs w="full">
-        <TabList>
-          <Tab>One</Tab>
-          <Tab>Two</Tab>
-          <Tab>Three</Tab>
-        </TabList>
-
-        <TabPanels>
-          <TabPanel></TabPanel>
-          <TabPanel>
-            <p>two!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>three!</p>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
+      <Wrap align="center">
+        <WrapItem>
+          <Avatar name="Dan Abrahmov" src="/Image/murilo.jpg" />
+        </WrapItem>
+        <Text color="white" fontWeight="bold" fontSize="18px">
+          Bem-vindo, Murilo
+        </Text>
+      </Wrap>
     </Flex>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,22 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { Flex, Button, Image, Box } from "@chakra-ui/react";
+import {
+  Flex,
+  Button,
+  Image,
+  Box,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+  Text,
+} from "@chakra-ui/react";
 import { useRouter } from "next/router";
-
-const batata = "testando";
 
 export default function HomeLogin() {
   const Router = useRouter();
@@ -11,19 +24,72 @@ export default function HomeLogin() {
     <>
       <Box />
       <Flex
-        bgImage="url(/Image/mosaic2.jpg)"
+        // bgImage="url(/Image/mosaic2.jpg)"
         bgPosition="center"
         bgRepeat="no-repeat"
         justify="center"
+        w="400px"
       >
-        Aqui vai ser o login
+        <TableContainer>
+          <Table variant="unstyled" size="sm" mt="1rem">
+            {/* <TableCaption>Imperial to metric conversion factors</TableCaption> */}
+            <Thead>
+              <Tr>
+                <Th>
+                  <Text align="center">Nome</Text>
+                </Th>
+                <Th>
+                  <Text align="center">Parcela</Text>
+                </Th>
+                <Th>
+                  <Text align="center">Valor</Text>
+                </Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>
+                  <Text
+                    align="center"
+                    bgColor="#D9D9D9"
+                    borderRadius="10px"
+                    p="5px"
+                  >
+                    Conta de água
+                  </Text>
+                </Td>
+                <Td>
+                  <Text
+                    align="center"
+                    bgColor="#D9D9D9"
+                    p="5px"
+                    borderRadius="10px"
+                  >
+                    1/12
+                  </Text>
+                </Td>
+                <Td>
+                  <Text
+                    align="center"
+                    bgColor="#D9D9D9"
+                    p="5px"
+                    borderRadius="10px"
+                  >
+                    R$ 800,00
+                  </Text>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+        {/* Aqui vai ser o login
         <Button
           onClick={() => {
             Router.push("/homepage");
           }}
         >
           Ir para a tela de home
-        </Button>
+        </Button> */}
       </Flex>
     </>
   );
