c7456a8803c5ab55b0e0f12a2164b7664b46da35
Murilo <murilo.ramos@rspneus.com.br>
2022-11-23 12:46:42 -0300

    chore: commit for testing

--- src/components/Layouts/layoutMobile.tsx
+++ src/components/Layouts/layoutMobile.tsx
@@ -15,7 +15,7 @@ export default function LayoutMob(props: any) {
         <Flex
           bgColor="#24293D"
           width="100vw"
-          h="100vh"
+          h="90vh"
           p="1rem"
           direction="column"
           justify="space-between"
